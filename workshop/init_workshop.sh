200b59d6ed4138e76a1e241779d2c02d2f68233f
Brandon Dold <brandondold@gmail.com>
2020-01-14 11:47:16 -0800

    adjust init script

--- workshop/init_workshop.sh
+++ workshop/init_workshop.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-# git clone https://github.com/brand161/serverless-object-recognition.git
+python3 -m venv rt-workshop; source rt-workshop/bin/activate
+cd rt-workshop
 export AWS_DEFAULT_REGION='us-west-2'
 export C9_EC2_ID=`aws ec2 describe-instances --region us-west-2 --filters Name=tag-key,Values='aws:cloud9:environment' Name=instance-state-name,Values='running' --query "Reservations[*].Instances[*].InstanceId" --output text`
+export BUCKET_NAME=`aws s3api list-buckets --query "reverse(sort_by(Buckets, &CreationDate))[:1] | [0].Name" | tr -d '"'`
 aws ec2 associate-iam-instance-profile --iam-instance-profile Name=RTWorkshopInstanceProfile --region us-west-2 --instance-id $C9_EC2_ID
-python3 -m venv tmp-workshop-dir; source tmp-workshop-dir/bin/activate
-cd tmp-workshop-dir
 pip --disable-pip-version-check install -q boto3
 pip --disable-pip-version-check install -q chalice
 pip --disable-pip-version-check install -q opencv-python
+
+#aws s3 sync . s3://$BUCKET_NAME
